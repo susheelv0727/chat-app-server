b57787c6dd0d229394c02e67961e7e49c0226045
susheelv0727 <susheelv07@gmail.com>
2025-09-01 15:20:58 -0500

    fix web socket error

--- chat-app-server/server.js
+++ chat-app-server/server.js
@@ -1,18 +1,19 @@
-// server.js (ESM)
-import { WebSocketServer, WebSocket } from 'ws';
 import http from 'http';
+import { WebSocketServer, WebSocket } from 'ws';
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('WebSocket server running');
+});
+
 const wss = new WebSocketServer({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
-  ws.on('message', (message) => {
-    const text = message.toString();
-
-    // Broadcast to ALL clients EXCEPT the sender
-    wss.clients.forEach((client) => {
+  ws.on('message', (msg) => {
+    const text = msg.toString();
+    wss.clients.forEach(client => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(text);
       }
@@ -22,4 +23,4 @@ wss.on('connection', (ws) => {
   ws.on('close', () => console.log('Client disconnected'));
 });
 
-server.listen(8080, () => console.log('Server running on ws://localhost:8080'));
+server.listen(8080, () => console.log('Server running on http://localhost:8080'));
